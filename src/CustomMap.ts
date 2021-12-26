39ddefcc5a7eea84293aa6a1b61ae612e388f428
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-12-27 00:19:23 +0600

    CustomMap.ts added

---
+++ src/CustomMap.ts
@@ -0,0 +1,13 @@
+export class CustomMap {
+  map: google.maps.Map;
+
+  constructor(divId: string) {
+    this.map = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Company } from "./Company";
-import { User } from "./User";
+// import { Company } from "./Company";
+// import { User } from "./User";
 
-const user = new User();
-const company = new Company();
+import { CustomMap } from "./CustomMap";
 
-console.log(user, company);
+// const user = new User();
+// const company = new Company();
+new CustomMap("map");
